bfe2bed5d511ae0323e8f2ff5d9c0217f502182a
Deepanshurao2001 <raodeepanshu99@gmail.com>
2023-04-17 22:31:39 +0530

    javascript  basics start

---
+++ javaScript/index.js
@@ -0,0 +1,20 @@
+
+console.log('jai Shree Ram!!')
+var a=100;
+console.log(a);
+a=120;
+console.log(a)
+let firstName='Deepanshu';
+console.log(firstName);
+
+firstName = 3;
+console.log(firstName);
+let num=1;
+let str='1';
+console.log(num == str);
+console.log(num === str);
+
+let num1=1;
+let str1=1;
+console.log(num1 == str1);
+console.log(num1 === str1);
