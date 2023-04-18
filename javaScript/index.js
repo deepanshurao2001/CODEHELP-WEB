ea5705c4cc536b1b92057c63568490eb2ec83c7f
Deepanshurao2001 <raodeepanshu99@gmail.com>
2023-04-18 23:28:48 +0530

    js

--- javaScript/index.js
+++ javaScript/index.js
@@ -18,3 +18,28 @@ let num1=1;
 let str1=1;
 console.log(num1 == str1);
 console.log(num1 === str1);
+
+ let rectangle1 = {
+     length: 9,
+     bredth: 4,
+
+     draw: function() {
+         console.log(this.length*this.bredth);
+     }
+
+
+ }
+
+//factor function
+function createRectangle(){
+    return rectangle={
+        length: 1,
+        breadth: 2,
+
+        draw(){
+            console.log('drawing rectangle');
+        }
+    };
+}
+
+let rectangleObj1 = createRectangle();
