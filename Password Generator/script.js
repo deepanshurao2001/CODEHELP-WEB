4679d94f17efa6b8c0d54cc7bccb4553c222627c
Deepanshurao2001 <raodeepanshu99@gmail.com>
2023-05-10 00:49:19 +0530

    complete password generator

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -27,12 +27,14 @@ setIndicator("#ccc");
 function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
-    //or kuch bhi karna chahiye ? - HW
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ((passwordLength - min)*100/(max - min)) + "% 100%"
 }
 
 function setIndicator(color) {
     indicator.style.backgroundColor = color;
-    //shadow - HW
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 function getRndInteger(min, max) {
