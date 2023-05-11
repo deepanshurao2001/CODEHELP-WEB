20de659b721fbd50e9d85ea4f897dcd3e2404d1f
Deepanshurao2001 <raodeepanshu99@gmail.com>
2023-05-11 23:13:41 +0530

    learning how to fetch API

---
+++ Weather application/index.js
@@ -0,0 +1,10 @@
+const API_key = "d1845658f92b31c64bd94f06f7188c9c";
+async function showWeather() {
+    let city = "goa";
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
+    const data = await response.json();
+    console.log("Weather data :->" + data);
+
+    let newpara = document.createElement('p');
+    newPara.textContent = 
+} 
